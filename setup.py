1f0ea03ebb30db7cbe6e6c92db432b2f1052f425
Nikita Grishko <grin.minsk@gmail.com>
2013-08-10 21:27:05 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+with open('.meta/packages') as reqs:
+    install_requires = reqs.read().split('\n')
+
+
+setup(
+    name='rpihelper',
+    version='0.0.1',
+    author='Nikita Grishko',
+    author_email='grin.minsk@gmail.com',
+    url='https://github.com/Gr1N/rpihelper',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
