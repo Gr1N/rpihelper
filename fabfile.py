65cd63c863d5c4f1cee27b210509ff3d9739052e
Nikita Grishko <grin.minsk@gmail.com>
2013-08-18 18:32:47 +0300

    Fabric power

---
+++ fabfile.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import os
+import time
+
+import yaml
+
+from fabric.api import task, run, cd
+from fabric.context_managers import prefix
+from fabric.contrib.files import exists, upload_template
+from fabric.utils import abort
+
+__all__ = (
+    'deploy',
+    'undeploy',
+)
+
+
+PROJECT = 'rpihelper'
+
+CONFIG_SHARED = '__shared'
+CONFIG_TEMPLATE_DIR = 'templates'
+
+# paths below are relative to project home
+VIRTUALENV_DIR = 'app/env'
+VIRTUALENV_ACTIVATE = '%s/bin/activate' % VIRTUALENV_DIR
+INITIAL_DIRS = (
+    'conf/rpihelper',
+    'conf/httpd',
+    'conf/nginx',
+)
+
+
+@task
+def deploy(config_file):
+    config = load_and_verify_config(config_file)
+    if config:
+        deploy_project(config)
+
+
+@task
+def undeploy(config_file):
+    config = load_and_verify_config(config_file)
+    if config:
+        undeploy_project(config)
+
+
+def load_and_verify_config(config_file):
+    with open(config_file, 'r') as stream:
+        config = yaml.load(stream)
+        project_config = config.get(PROJECT)
+        if not project_config:
+            abort('No configuration for project "%s" in "%s"' % (PROJECT, config_file))
+
+        home = project_config.get('home')
+        if not home:
+            abort('Empty "home" for project "%s" in "%s"' % (PROJECT, config_file))
+
+        # dirty but ensures safety for system
+        root = config[CONFIG_SHARED]['root']
+        assert home.startswith(root)
+        assert ' ' not in home
+
+        config['app'] = project_config
+        config['PROJECT'] = PROJECT
+
+        config['TIMESTAMP'] = time.mktime(datetime.utcnow().utctimetuple())
+
+        return config
+
+
+def deploy_project(config):
+    home = config['app']['home']
+    ensure_environment(home, config)
+    deploy_project_package(home, config)
+    deploy_configurations(home, config)
+
+
+def undeploy_project(config):
+    home = config['app']['home']
+    destroy_environment(home)
+
+
+def ensure_environment(home, config):
+    if not exists(home):
+        run('mkdir "%s"' % home)
+
+    ensure_virtualenv(home, config)
+    ensure_layout(home)
+
+
+def destroy_environment(home):
+    if exists(home):
+        run('rm -rf "%s"' % home)
+
+
+def ensure_virtualenv(home, config):
+    with cd(home):
+        if exists(VIRTUALENV_ACTIVATE):
+            return
+
+        python = config[CONFIG_SHARED]['PYTHON']
+        run('virtualenv "%s" --distribute --python=%s' % (VIRTUALENV_DIR, python))
+
+
+def ensure_layout(home):
+    with cd(home):
+        run('mkdir -p "%s"' % '" "'.join(INITIAL_DIRS))
+
+
+def deploy_project_package(home, config):
+    with cd(home):
+        with prefix('source %s' % VIRTUALENV_ACTIVATE):
+            run('pip install %s' % config['app']['pipgit'])
+
+
+def deploy_configurations(home, config):
+    for filename in config[CONFIG_SHARED]['configurations']:
+        template = os.path.join(CONFIG_TEMPLATE_DIR, filename)
+        if not os.path.exists(template):
+            continue
+
+        destination = os.path.join(home, filename)
+        upload_template(template, destination, context=config, use_jinja=True)
--- templates/app/app.wsgi
+++ templates/app/app.wsgi
@@ -7,6 +7,9 @@ import sys
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 
 
+os.environ['FLASK_CONFIG_FILE'] = '{{ app.home }}conf/rpihelper/config.yaml'
+
+
 activate_this = os.path.join(BASE_DIR, 'env/bin/activate_this.py')
 exec(compile(open(activate_this).read(), activate_this, 'exec'), dict(__file__=activate_this))
 
