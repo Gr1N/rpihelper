36bda3954faa5dc71eac655a63565a1437c9bc32
Nikita Grishko <grin.minsk@gmail.com>
2014-01-19 22:56:31 +0300

    Switch version to 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('.meta/packages') as reqs:
 
 setup(
     name='rpihelper',
-    version='0.0.2',
+    version='0.0.3',
     author='Nikita Grishko',
     author_email='grin.minsk@gmail.com',
     url='https://github.com/Gr1N/rpihelper',
